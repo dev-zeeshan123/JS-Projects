1eea886a98f4c7565d24897a905184997b026002
dev-zeeshan123 <dev.khanzeeshan@gmail.com>
2025-04-29 07:51:47 +0000

    conversion done

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,24 @@
+let score = "zeeshan"
+//let point = 102;
+//console.log(typeof score) /*In JavaScript, typeof is an operator used to determine the type of a value or variable. It returns a string indicating the type.*/
+//let valueInNumber =  Number(score)
+//console.log(typeof valueInNumber)
+//console.log(typeof(score))
+//console.log(typeof point)
+
+// 33 => 33
+//"33abc"=> NaN(Not a Number)
+//true => 1 false => 0
+let isloggedIn = "zeeshan"
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+//console.log(stringNumber)
+console.log(typeof stringNumber)
+ 
