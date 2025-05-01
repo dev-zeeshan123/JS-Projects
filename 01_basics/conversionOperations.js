baffea8417d4b514f6cb83fa445cda597a646004
dev-zeeshan123 <dev.khanzeeshan@gmail.com>
2025-05-01 03:35:50 +0000

    comaprision & Updated ConversionOperation done

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -9,16 +9,43 @@ let score = "zeeshan"
 // 33 => 33
 //"33abc"=> NaN(Not a Number)
 //true => 1 false => 0
-let isloggedIn = "zeeshan"
-let booleanIsLoggedIn = Boolean(isloggedIn)
-console.log(booleanIsLoggedIn)
+//let isloggedIn = "zeeshan"
+//let booleanIsLoggedIn = Boolean(isloggedIn)
+//console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
+//let someNumber = 33
+//let stringNumber = String(someNumber)
 //console.log(stringNumber)
-console.log(typeof stringNumber)
- 
+//console.log(typeof stringNumber)
+//********************Operations******************
+ let value = 3 
+ let negValue = -value
+ /*console.log(negValue)
+ console.log(2+2)
+ console.log(2-2)
+ console.log(2*2)
+ console.log(2**3)
+ console.log(2/3)*/
+ //console.log(2%3)
+ let str1 = "hello"
+ let str2 = " Zeeshan"
+ let str3 = str1+str2
+//console.log(str3)
+//console.log("1"+1)
+//console.log(1+"2")
+//console.log("1"+2+2)
+//console.log(1+2+"2")
+console.log((3+4)*5%3)
+console.log(+ true)
+console.log(+" ")
+let num1 , num2 , num3
+num1 = num2 = num3=2+2
+console.log(num1)
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
