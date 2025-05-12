04d747571f289519bce3b05d8bb26138ff2ba0d9
dev-zeeshan123 <dev.khanzeeshan@gmail.com>
2025-05-12 11:14:58 +0000

    udate commit

--- 01_basics/date_and_time.js
+++ 01_basics/date_and_time.js
@@ -1,37 +1,39 @@
 // Dates
 
 let myDate = new Date()
-// console.log(myDate.toString());Fri May 02 2025 05:32:44 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toString());//Fri May 02 2025 05:32:44 GMT+0000 (Coordinated Universal Time)
 // console.log(myDate.toDateString());//Fri May 02 2025
  console.log(myDate.toLocaleString());
-// console.log(typeof myDate); // object type 
+console.log(typeof myDate); // object type 
+console.log(myDate.toLocaleString('en-US',{timeZone : 'Asia/Kolkata'}));
 
-let myCreatedDate1 = new Date(2023, 0, 23)
-let myCreatedDate2 = new Date(2023, 0, 23, 5, 3)
-let myCreatedDate3 = new Date("2023-01-14")
-let myCreatedDate4 = new Date("01-14-2023")
-console.log(myCreatedDate1.toLocaleString());
-console.log(myCreatedDate2.toLocaleString());
-console.log(myCreatedDate3.toLocaleString());
-console.log(myCreatedDate4.toLocaleString());
 
-let myTimeStamp = Date.now()
+// let myCreatedDate1 = new Date(2023, 0, 23)
+// let myCreatedDate2 = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate3 = new Date("2023-01-14")
+// let myCreatedDate4 = new Date("01-14-2023")
+// console.log(myCreatedDate1.toLocaleString());
+// console.log(myCreatedDate2.toLocaleString());
+// console.log(myCreatedDate3.toLocaleString());
+// console.log(myCreatedDate4.toLocaleString());
 
-console.log(myTimeStamp);
-console.log(myCreatedDate1.getTime());
-console.log(Math.floor(Date.now()/1000));
+// let myTimeStamp = Date.now()
 
-let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+// console.log(myTimeStamp);
+// console.log(myCreatedDate1.getTime());
+// console.log(Math.floor(Date.now()/1000));
 
-`${newDate.getDay()} and the time `
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
 
-newDate.toLocaleString('default', {
-    weekday: "long",
+// `${newDate.getDay()} and the time `
+
+// newDate.toLocaleString('default', {
+//     weekday: "long",
     
-})
+// })
 /*
 output
 5/2/2025, 6:42:56 AM
